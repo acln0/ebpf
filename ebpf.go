2dcdf13761f4fac30c5edeaf7d7c3db9a5846f14
Andrei Tudor Călin <mail@acln.ro>
2018-11-02 11:26:48 +0100

    add basic definitions and low level bits
    Declare and define bpf(2) commands, add SyscallError type to capture
    errors from bpf(2) calls, add low level hooks for system calls.
    
    Signed-off-by: Andrei Tudor Călin <mail@acln.ro>

---
+++ ebpf.go
@@ -0,0 +1,186 @@
+// Copyright 2018 Andrei Tudor Călin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebpf
+
+import (
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+// bpf(2) commands.
+const (
+	cmdMapCreate = iota
+	cmdMapLookup
+	cmdMapUpdate
+	cmdMapDelete
+	cmdMapGetNextKey
+	cmdProgLoad
+	cmdObjPin
+	cmdObjGet
+	cmdProgAttach
+	cmdProgDetach
+	cmdProgTestRun
+	cmdProgGetNextID
+	cmdMapGetNextID
+	cmdProgGetFDByID
+	cmdMapGetFDByID
+	cmdObjGetInfoByFD
+	cmdProgQuery
+	cmdRawTracepointOpen
+	cmdBTFLoad
+	cmdBTFGetFDByID
+	cmdTaskFDQuery
+)
+
+func validCommand(cmd int) bool {
+	return cmd >= cmdMapCreate && cmd <= cmdTaskFDQuery
+}
+
+var commandNames = [...]string{
+	cmdMapCreate:         "BPF_MAP_CREATE",
+	cmdMapLookup:         "BPF_MAP_LOOKUP_ELEM",
+	cmdMapUpdate:         "BPF_MAP_UPDATE_ELEM",
+	cmdMapDelete:         "BPF_MAP_DELETE_ELEM",
+	cmdMapGetNextKey:     "BPF_MAP_GET_NEXT_KEY",
+	cmdProgLoad:          "BPF_PROG_LOAD",
+	cmdObjPin:            "BPF_OBJ_PIN",
+	cmdObjGet:            "BPF_OBJ_GET",
+	cmdProgAttach:        "BPF_PROG_ATTACH",
+	cmdProgDetach:        "BPF_PROG_DETACH",
+	cmdProgTestRun:       "BPF_PROG_TEST_RUN",
+	cmdProgGetNextID:     "BPF_PROG_GET_NEXT_ID",
+	cmdMapGetNextID:      "BPF_MAP_GET_NEXT_ID",
+	cmdProgGetFDByID:     "BPF_PROG_GET_FD_BY_ID",
+	cmdMapGetFDByID:      "BPF_MAP_GET_FD_BY_ID",
+	cmdObjGetInfoByFD:    "BPF_OBJ_GET_INFO_BY_FD",
+	cmdProgQuery:         "BPF_PROG_QUERY",
+	cmdRawTracepointOpen: "BPF_RAW_TRACEPOINT_OPEN",
+	cmdBTFLoad:           "BPF_BTF_LOAD",
+	cmdBTFGetFDByID:      "BPF_BTF_GET_FD_BY_ID",
+	cmdTaskFDQuery:       "BPF_TASK_FD_QUERY",
+}
+
+func commandString(cmd int) string {
+	if !validCommand(cmd) {
+		return "UNKNOWN"
+	}
+	return commandNames[cmd]
+}
+
+// SyscallError records an error from a bpf(2) system call.
+//
+// Cmd is a string describing the bpf command executed, e.g.
+// "BPF_CREATE_MAP".
+//
+// Err is the underlying error, of type syscall.Errno.
+type SyscallError struct {
+	Cmd string
+	Err error
+}
+
+func (e *SyscallError) Error() string {
+	return e.Cmd + ": " + e.Err.Error()
+}
+
+// Cause returns the cause of the error: e.Err.
+func (e *SyscallError) Cause() error {
+	return e.Err
+}
+
+// wrapSyscallError wraps err in a *SyscallError. For convenience, if err
+// is nil, wrapSyscallError returns nil.
+func wrapSyscallError(cmd int, err error) error {
+	if err == nil {
+		return nil
+	}
+	return &SyscallError{Cmd: commandString(cmd), Err: err}
+}
+
+// objectName is a null-terminated string, at most 15 bytes long.
+type objectName [16]byte
+
+// newObjectName creates a new object name from s. s must not contain null
+// bytes. If s is longer than 15 bytes, tailing bytes are truncated.
+func newObjectName(s string) objectName {
+	var name objectName
+	n := copy(name[:], s)
+	name[n-1] = 0
+	return name
+}
+
+type causer interface {
+	Cause() error
+}
+
+// Error introspection routines.
+
+// IsExist returns a boolean indicating whether err reports that
+// an object (e.g. an entry in a map) already exists.
+func IsExist(err error) bool {
+	if c, ok := err.(causer); ok {
+		return c.Cause() == unix.EEXIST
+	}
+	return false
+}
+
+// IsNotExist returns a boolean indicating whether err is reports
+// that an object (e.g. an entry in a map) does not exist.
+func IsNotExist(err error) bool {
+	if c, ok := err.(causer); ok {
+		return c.Cause() == unix.ENOENT
+	}
+	return false
+}
+
+// IsTooBig returns a boolean indicating whether err is known
+// to report that a map has reached its size limit.
+func IsTooBig(err error) bool {
+	if c, ok := err.(causer); ok {
+		return c.Cause() == unix.E2BIG
+	}
+	return false
+}
+
+// Low level system call hooks.
+
+// closeFunc hooks the close(2) system call.
+var closeFunc = unix.Close
+
+// bpfFunc hooks the bpf(2) system call.
+var bpfFunc = sysBPF
+
+// sysBPF calls bpf(2) with the specified arguments. It executes the command
+// cmd with attributes attr. size must be unsafe.Sizeof the object attr is
+// pointing to.
+func sysBPF(cmd uintptr, attr unsafe.Pointer, size uintptr) (int, error) {
+	r, _, e := unix.Syscall(unix.SYS_BPF, cmd, uintptr(attr), size)
+	if e != 0 {
+		return int(r), e
+	}
+	return int(r), nil
+}
+
+// Low level pointer-wrangling routines.
+
+// uint32Bytes creates a []byte b such that &b[0] is i.
+func uint32Bytes(i *uint32) []byte {
+	return (*[4]byte)(unsafe.Pointer(i))[:]
+}
+
+// bptr creates a u64ptr which carries &b[0].
+func bptr(b []byte) u64ptr {
+	return u64ptr{p: unsafe.Pointer(&b[0])}
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module acln.ro/ebpf
+
+require golang.org/x/sys v0.0.0-20181031143558-9b800f95dbbc
