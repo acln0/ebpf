57e7d72bad9a2fb4c83f75f99d820638be5967b5
Andrei Tudor Călin <mail@acln.ro>
2018-11-07 13:11:50 +0100

    map: redesign API, refactor
    Specific map types such as Hashmap and Array don't really work well.
    Callers probably have to write wrapper types around them anyway.
    Remove these types and, instead, use a single Map type, which
    encapsulates all of the possible maps. This way, we cannot enforce
    a few additional invariants, but callers have to do that anyway.
    So let's be explicit about it and offer the real API.
    
    The Sysfd kludge remains for now, as the rest of the API is figured
    out. Some bits (e.g. BTF) are missing, because I know nothing about
    them for now.
    
    Signed-off-by: Andrei Tudor Călin <mail@acln.ro>

--- map.go
+++ map.go
@@ -26,30 +26,167 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// MapType is the type of an eBPF map.
+type MapType uint32
+
 // Supported map types.
 const (
-	mapUnspec = iota
-	mapHash
-	mapArray
-	mapProgArray
-	mapPerfEventArray
-	mapPerCPUHash
-	mapPerCPUArray
-	mapStackTrace
-	mapCGroupArray
-	mapLRUHash
-	mapLRUPerCPUHash
-	mapLPMTrie
-	mapArrayOfMaps
-	mapHashOfMaps
-	mapDevmap
-	mapSockmap
-	mapCPUmap
-	mapXSKMap
-	mapSockhash
-	mapCGroupStorage
-	mapReuseportSockarray
+	MapUnspec MapType = iota
+
+	MapHash
+	MapArray
+	MapProgArray
+	MapPerfEventArray
+	MapPerCPUHash
+	MapPerCPUArray
+	MapStackTrace
+	MapCGroupArray
+	MapLRUHash
+	MapLRUPerCPUHash
+	MapLPMTrie
+	MapArrayOfMaps
+	MapHashOfMaps
+	MapDevmap
+	MapSockmap
+	MapCPUmap
+	MapXSKMap
+	MapSockhash
+	MapCGroupStorage
+	MapReuseportSockarray
+
+	maxMapType // keep at the end
 )
+
+// Map configures an eBPF map.
+//
+// Before use, a Map must be initialized using the Init method. KeySize,
+// and MaxEntries must be non-zero. For arrays, KeySize must be 4.
+//
+// TODO(acln): investigate if ValueSize can be zero.
+//
+// ObjectName names the map. Names must not contain the NULL character. Names
+// longer than 15 bytes are truncated.
+//
+// A Map must not be copied after initialization. After initialization,
+// it is safe (in the data race sense) to call methods on the Map from
+// multiple goroutines concurrently. However, it may not always be safe to
+// do so from the perspective of the actual eBPF map semantics. For example,
+// writes to hash maps are atomic, while writes to arrays are not. Consult
+// the bpf documentation for more details.
+//
+// TODO(acln): eitehr expand on where to find said documentation, or document
+// the semantics more precisely.
+type Map struct {
+	Type           MapType
+	KeySize        uint32
+	ValueSize      uint32
+	MaxEntries     uint32
+	Flags          uint32 // TODO(acln): investigate these
+	InnerMap       *Map   // TODO(acln): is this right?
+	NUMANode       uint32
+	ObjectName     string
+	InterfaceIndex uint32 // TODO(acln): document this
+
+	// TODO(acln): add the BTF bits at some point
+
+	// fd is the inner map FD. It is a pointer, to prevent callers
+	// from copying a Map and sweeping the actual FD from under us.
+	fd *mapFD
+}
+
+// Init initializes the map.
+func (m *Map) Init() error {
+	if m.Type == MapUnspec {
+		return errors.New("ebpf: unspecified map type")
+	}
+	if m.Type >= maxMapType {
+		return fmt.Errorf("ebpf: invalid map type %d", m.Type)
+	}
+	if m.KeySize == 0 {
+		return errors.New("ebpf: map KeySize not configured")
+	}
+	if m.MaxEntries == 0 {
+		return errors.New("ebpf: map MaxEntries not configured")
+	}
+	cfg := mapConfig{
+		Type:           m.Type,
+		KeySize:        m.KeySize,
+		ValueSize:      m.ValueSize,
+		MaxEntries:     m.MaxEntries,
+		Flags:          m.Flags,
+		InnerMapFD:     0, // TODO(acln): fix this
+		NUMANode:       m.NUMANode,
+		Name:           newObjectName(m.ObjectName),
+		InterfaceIndex: m.InterfaceIndex,
+	}
+	fd := new(mapFD)
+	if err := fd.Init(&cfg); err != nil {
+		return wrapMapOpError("init", err)
+	}
+	m.fd = fd
+	return nil
+}
+
+// Lookup looks up the value for k and stores it in v. If k is not found
+// in the map, Lookup returns an error such that IsNotExist(err) == true.
+func (m *Map) Lookup(k, v []byte) error {
+	return wrapMapOpError("lookup", m.fd.Lookup(k, v))
+}
+
+// Set sets the value for k to v. If an entry for k exists in the map,
+// it will be overwritten.
+func (m *Map) Set(k, v []byte) error {
+	return wrapMapOpError("set", m.fd.Update(k, v, mapUpdateAny))
+}
+
+// Create creates a new entry for k in the map, and sets the value to v.
+// If an entry for k exists in the map, Create returns an error such that
+// IsExist(err) == true.
+func (m *Map) Create(k, v []byte) error {
+	return wrapMapOpError("create", m.fd.Update(k, v, mapUpdateNoexist))
+}
+
+// Update updates the entry for k to v. If an entry for k does not exist in
+// the map, Update returns an error such that IsNotExist(err) == true.
+func (m *Map) Update(k, v []byte) error {
+	return wrapMapOpError("update", m.fd.Update(k, v, mapUpdateExist))
+}
+
+// Delete deletes the entry for k. If an entry for k does not exist in the
+// map, Delete returns an error such that IsNotExist(err) == true.
+func (m *Map) Delete(k []byte) error {
+	return wrapMapOpError("delete", m.fd.Delete(k))
+}
+
+// Iterate iterates over all keys in the map and calls fn for each key-value
+// pair. If fn returns true or the final element of the map is reached,
+// iteration stops. fn must not retain the arguments it is called with.
+//
+// startHint optionally specifies a key that does *not* exist in the map, such
+// that Iterate can begin iteration from the first key that does. Due to the
+// nature of BPF map iterators, on Linux kernels older than 4.12, Iterate
+// requires a non-nil startHint. On Linux >= 4.12, startHint may be nil, but
+// it is recommended to pass a valid one nevertheless.
+func (m *Map) Iterate(fn func(k, v []byte) (stop bool), startHint []byte) error {
+	return wrapMapOpError("iterate", m.fd.Iterate(fn, startHint))
+}
+
+// Close destroys the map and releases the associated file descriptor. After a call
+// to Close, future method calls on the Map will return errors.
+func (m *Map) Close() error {
+	return m.fd.Close()
+}
+
+// Sysfd is a horrible kludge that exists only temporarily. A better interface
+// should exist instead.
+//
+// TODO(acln): delete this as soon as possible.
+func (m *Map) Sysfd() int {
+	sysfd, _ := m.fd.fd.Incref()
+	m.fd.fd.Decref()
+
+	return sysfd
+}
 
 // mapFD is a low level wrapper around a bpf map file descriptor.
 type mapFD struct {
@@ -59,87 +196,7 @@ type mapFD struct {
 	maxEntries uint32
 }
 
-// Init creates a map as specified by cfg and, if successful, initializes
-// m to refer to the new map.
 func (m *mapFD) Init(cfg *mapConfig) error {
-	return wrapMapOpError("init", m.init(cfg))
-}
-
-// Lookup looks up the value for k and stores it in v. If k is not found
-// in the map, Lookup returns an error such that IsNotExist(err) == true.
-func (m *mapFD) Lookup(k, v []byte) error {
-	return wrapMapOpError("lookup", m.lookup(k, v))
-}
-
-// EnsureAccessInBounds checks that the index i is in bounds, under the
-// assumption that mfd refers to a BPF array.
-func (m *mapFD) EnsureAccessInBounds(i uint32) error {
-	if _, err := m.fd.Incref(); err != nil {
-		return err
-	}
-	defer m.fd.Decref()
-
-	if i >= m.maxEntries {
-		return &oobError{i: i, b: m.maxEntries}
-	}
-	return nil
-}
-
-// oobError is an out-of-bounds array access error. Since this is a programmer
-// error, it is not actionable, it cannot be introspected, and the type is
-// not exported.
-type oobError struct {
-	i, b uint32
-}
-
-func (e *oobError) Error() string {
-	return fmt.Sprintf("array access at index %d out of bounds (%d)", e.i, e.b)
-}
-
-// Set sets the value for k to v. If an entry for k exists in the map,
-// it will be overwritten.
-func (m *mapFD) Set(k, v []byte) error {
-	return wrapMapOpError("set", m.update(k, v, mapUpdateAny))
-}
-
-// Create creates a new entry for k in the map, and sets the value to v.
-// If an entry for k exists in the map, Create returns an error such that
-// IsExist(err) == true.
-func (m *mapFD) Create(k, v []byte) error {
-	return wrapMapOpError("create", m.update(k, v, mapUpdateNoexist))
-}
-
-// Update updates the entry for k to v. If an entry for k does not exist in
-// the map, Update returns an error such that IsNotExist(err) == true.
-func (m *mapFD) Update(k, v []byte) error {
-	return wrapMapOpError("update", m.update(k, v, mapUpdateExist))
-}
-
-// Delete deletes the entry for k. If an entry for k does not exist in the
-// map, Delete returns an error such that IsNotExist(err) == true.
-func (m *mapFD) Delete(k []byte) error {
-	return wrapMapOpError("delete", m.delete(k))
-}
-
-// Iterate iterates over all keys in the map and calls fn for each key-value
-// pair. If fn returns true or the final element of the map is reached,
-// iteration stops. fn must not retain the arguments it is called with.
-//
-// startHint optionally specifies a key that does *not* exist in the map,
-// such that Iterate can begin iteration from the first key that does. Due
-// to the nature of BPF map iterators, on Linux kernels older than 4.12,
-// Iterate requires a non-nil startHint. On Linux >= 4.12, startHint may be
-// nil, but it is recommended to pass a valid one nevertheless.
-func (m *mapFD) Iterate(fn func(k, v []byte) (stop bool), startHint []byte) error {
-	return wrapMapOpError("iterate", m.iterate(fn, startHint))
-}
-
-// Close destroys the map and releases its associated file descriptor.
-func (m *mapFD) Close() error {
-	return m.fd.Close()
-}
-
-func (m *mapFD) init(cfg *mapConfig) error {
 	sysfd, err := sysCreateMap(cfg)
 	if err != nil {
 		return err
@@ -153,7 +210,7 @@ func (m *mapFD) init(cfg *mapConfig) error {
 	return nil
 }
 
-func (m *mapFD) lookup(k, v []byte) error {
+func (m *mapFD) Lookup(k, v []byte) error {
 	sysfd, err := m.fd.Incref()
 	if err != nil {
 		return err
@@ -166,7 +223,14 @@ func (m *mapFD) lookup(k, v []byte) error {
 	return sysLookup(sysfd, k, v)
 }
 
-func (m *mapFD) update(k, v []byte, flag uint64) error {
+// Flags for map update operations.
+const (
+	mapUpdateAny     = iota // BPF_UPDATE_ANY
+	mapUpdateNoexist        // BPF_UPDATE_NOEXIST
+	mapUpdateExist          // BPF_UPDATE_EXIST
+)
+
+func (m *mapFD) Update(k, v []byte, flag uint64) error {
 	sysfd, err := m.fd.Incref()
 	if err != nil {
 		return err
@@ -179,7 +243,7 @@ func (m *mapFD) update(k, v []byte, flag uint64) error {
 	return sysUpdate(sysfd, k, v, flag)
 }
 
-func (m *mapFD) delete(k []byte) error {
+func (m *mapFD) Delete(k []byte) error {
 	sysfd, err := m.fd.Incref()
 	if err != nil {
 		return err
@@ -192,7 +256,7 @@ func (m *mapFD) delete(k []byte) error {
 	return sysDelete(sysfd, k)
 }
 
-func (m *mapFD) iterate(fn func(k, v []byte) bool, startHint []byte) error {
+func (m *mapFD) Iterate(fn func(k, v []byte) bool, startHint []byte) error {
 	sysfd, err := m.fd.Incref()
 	if err != nil {
 		return err
@@ -223,25 +287,8 @@ func (m *mapFD) iterate(fn func(k, v []byte) bool, startHint []byte) error {
 	}
 }
 
-// ensureCorrectKeyValueSize returns an error if len(k) and len(v) don't
-// match the key and value sizes the mapFD was configured with.
-func (m *mapFD) ensureCorrectKeyValueSize(k, v []byte) error {
-	if err := m.ensureCorrectKeySize(k); err != nil {
-		return err
-	}
-	if len(v) != m.valueSize {
-		return m.valueSizeError(len(v))
-	}
-	return nil
-}
-
-// ensureCorrectKeyValueSize returns an error if len(k) doesn't match the
-// key size the mapFD was configured with.
-func (m *mapFD) ensureCorrectKeySize(k []byte) error {
-	if len(k) != m.keySize {
-		return m.keySizeError(len(k))
-	}
-	return nil
+func (m *mapFD) Close() error {
+	return m.fd.Close()
 }
 
 // argumentSizeError records an error a mismatch between the size of a key
@@ -262,12 +309,29 @@ func (e *argumentSizeError) Error() string {
 	return fmt.Sprintf("%s size is %d, want %d", e.arg, e.got, e.want)
 }
 
-func (m *mapFD) keySizeError(got int) error {
-	return &argumentSizeError{arg: "key", got: got, want: m.keySize}
+func (m *mapFD) ensureCorrectKeyValueSize(k, v []byte) error {
+	if err := m.ensureCorrectKeySize(k); err != nil {
+		return err
+	}
+	if len(v) != m.valueSize {
+		return &argumentSizeError{
+			arg:  "value",
+			got:  len(v),
+			want: m.valueSize,
+		}
+	}
+	return nil
 }
 
-func (m *mapFD) valueSizeError(got int) error {
-	return &argumentSizeError{arg: "value", got: got, want: m.valueSize}
+func (m *mapFD) ensureCorrectKeySize(k []byte) error {
+	if len(k) != m.keySize {
+		return &argumentSizeError{
+			arg:  "key",
+			got:  len(k),
+			want: m.keySize,
+		}
+	}
+	return nil
 }
 
 // Low level attribute structures and system call wrappers.
@@ -283,7 +347,7 @@ func sysCreateMap(cfg *mapConfig) (int, error) {
 // mapConfig configures a BPF map. It specifies the parameters for a
 // BPF_MAP_CREATE operation.
 type mapConfig struct {
-	Type           uint32
+	Type           MapType
 	KeySize        uint32
 	ValueSize      uint32
 	MaxEntries     uint32
@@ -329,13 +393,6 @@ func sysUpdate(sysfd int, k, v []byte, flag uint64) error {
 	_, err := bpfFunc(cmd, unsafe.Pointer(&p), unsafe.Sizeof(p))
 	return wrapSyscallError(cmd, err)
 }
-
-// Flags for map update operations.
-const (
-	mapUpdateAny     = iota // BPF_UPDATE_ANY
-	mapUpdateNoexist        // BPF_UPDATE_NOEXIST
-	mapUpdateExist          // BPF_UPDATE_EXIST
-)
 
 // mapUpdateParams specifies parameters for a BPF_MAP_UPDATE_ELEM operation.
 type mapUpdateParams struct {
--- map_test.go
+++ map_test.go
@@ -17,6 +17,7 @@ package ebpf
 import (
 	"fmt"
 	"io"
+	"reflect"
 	"runtime"
 	"strings"
 	"sync"
@@ -25,6 +26,339 @@ import (
 
 	"acln.ro/rc"
 )
+
+func TestHashmap(t *testing.T) {
+	revert := trackMapFDs(t)
+	defer revert()
+
+	t.Run("Lookup", testHashmapLookup)
+	t.Run("Set", testHashmapSet)
+	t.Run("Update", testHashmapUpdate)
+	t.Run("Create", testHashmapCreate)
+	t.Run("Iterate", testHashmapIterate)
+	t.Run("Delete", testHashmapDelete)
+	t.Run("E2BIG", testHashmapE2BIG)
+}
+
+func testHashmapLookup(t *testing.T) {
+	h := newUint64Hashmap(t, 4)
+	defer h.Close()
+
+	k, v := uint64(4), uint64(8)
+	if err := h.Set(k, v); err != nil {
+		t.Fatal(err)
+	}
+	got, err := h.Lookup(k)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != v {
+		t.Fatalf("Lookup(%d): got %d, want %d", k, got, v)
+	}
+	k = uint64(123)
+	got, err = h.Lookup(k)
+	if err == nil {
+		t.Fatalf("Lookup(%d) succeeded: got %x", k, got)
+	}
+	if !IsNotExist(err) {
+		t.Fatalf("IsNotExist(%#v (%q)) == false, want true", err, err.Error())
+	}
+}
+
+func testHashmapSet(t *testing.T) {
+	h := newUint64Hashmap(t, 4)
+	defer h.Close()
+
+	k, v := uint64(15), uint64(16)
+	if err := h.Set(k, v); err != nil {
+		t.Fatal(err)
+	}
+	got, err := h.Lookup(k)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != v {
+		t.Fatalf("Lookup(%d): got %d, want %d", k, got, v)
+	}
+	v = uint64(23)
+	if err := h.Set(k, v); err != nil {
+		t.Fatal(err)
+	}
+	got, err = h.Lookup(k)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != v {
+		t.Fatalf("Lookup(%d): got %d, want %d", k, got, v)
+	}
+}
+
+func testHashmapUpdate(t *testing.T) {
+	h := newUint64Hashmap(t, 4)
+	defer h.Close()
+
+	k, v := uint64(3), uint64(50)
+	if err := h.Set(k, v); err != nil {
+		t.Fatal(err)
+	}
+	v = uint64(51)
+	if err := h.Update(k, v); err != nil {
+		t.Fatal(err)
+	}
+	got, err := h.Lookup(k)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != v {
+		t.Fatalf("Lookup(%d): got %d, want %d", k, got, v)
+	}
+	k = uint64(5)
+	err = h.Update(k, v)
+	if err == nil {
+		t.Fatalf("succeeded for non-existent key")
+	}
+	if !IsNotExist(err) {
+		t.Fatalf("IsNotExist(%#v (%q)) == false, want true", err, err.Error())
+	}
+}
+
+func testHashmapCreate(t *testing.T) {
+	h := newUint64Hashmap(t, 4)
+	defer h.Close()
+
+	k, v := uint64(23), uint64(42)
+	if err := h.Create(k, v); err != nil {
+		t.Fatal(err)
+	}
+	v = uint64(59)
+	err := h.Create(k, v)
+	if err == nil {
+		t.Fatalf("succeeded for existing key")
+	}
+	if !IsExist(err) {
+		t.Fatalf("IsExist(%#v (%q)) == false, want true", err, err.Error())
+	}
+}
+
+func testHashmapIterate(t *testing.T) {
+	h := newUint64Hashmap(t, 8)
+	defer h.Close()
+
+	pairs := map[uint64]uint64{
+		4:  8,
+		15: 16,
+		23: 42,
+	}
+	for k, v := range pairs {
+		if err := h.Set(k, v); err != nil {
+			t.Fatal(err)
+		}
+	}
+	hint := ^uint64(0)
+	seen := map[uint64]uint64{}
+	fn := func(k, v uint64) bool {
+		seen[k] = v
+		return false
+	}
+	if err := h.Iterate(fn, hint); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(pairs, seen) {
+		t.Fatalf("inserted %v, saw %v", pairs, seen)
+	}
+}
+
+func testHashmapDelete(t *testing.T) {
+	h := newUint64Hashmap(t, 4)
+	defer h.Close()
+
+	k, v := uint64(10), uint64(20)
+	if err := h.Create(k, v); err != nil {
+		t.Fatal(err)
+	}
+	if err := h.Delete(k); err != nil {
+		t.Fatal(err)
+	}
+	got, err := h.Lookup(k)
+	if err == nil {
+		t.Fatalf("Lookup(%d) succeeded after Delete(%d): got %d", k, k, got)
+	}
+	err = h.Delete(k)
+	if err == nil {
+		t.Fatalf("succeeded for non-existent key")
+	}
+	if !IsNotExist(err) {
+		t.Fatalf("IsNotExist(%#v (%q)) == false, want true", err, err.Error())
+	}
+}
+
+func testHashmapE2BIG(t *testing.T) {
+	const size = 16
+	h := newUint64Hashmap(t, size)
+	defer h.Close()
+
+	v := uint64(100)
+	for i := 0; i < size; i++ {
+		k := uint64(i)
+		if err := h.Create(k, v); err != nil {
+			t.Fatal(err)
+		}
+	}
+	err := h.Create(uint64(size), v)
+	if err == nil {
+		t.Fatalf("Create succeeded on map at size limit")
+	}
+	if !IsTooBig(err) {
+		t.Fatalf("IsTooBig(%#v (%q)) == false, want true", err, err.Error())
+	}
+}
+
+func newUint64Hashmap(t *testing.T, maxEntries uint32) *uint64Hashmap {
+	t.Helper()
+	m := &Map{
+		Type:       MapHash,
+		KeySize:    uint32(unsafe.Sizeof(uint64(0))),
+		ValueSize:  uint32(unsafe.Sizeof(uint64(0))),
+		MaxEntries: maxEntries,
+		ObjectName: "test_map",
+	}
+	if err := m.Init(); err != nil {
+		t.Fatal(err)
+	}
+	return &uint64Hashmap{m: m}
+}
+
+type uint64Hashmap struct {
+	m *Map
+}
+
+func (h *uint64Hashmap) Lookup(k uint64) (v uint64, err error) {
+	err = h.m.Lookup(uint64b(&k), uint64b(&v))
+	return v, err
+}
+
+func (h *uint64Hashmap) Set(k, v uint64) error {
+	return h.m.Set(uint64b(&k), uint64b(&v))
+}
+
+func (h *uint64Hashmap) Create(k, v uint64) error {
+	return h.m.Create(uint64b(&k), uint64b(&v))
+}
+
+func (h *uint64Hashmap) Update(k, v uint64) error {
+	return h.m.Update(uint64b(&k), uint64b(&v))
+}
+
+func (h *uint64Hashmap) Iterate(fn func(k, v uint64) bool, hint uint64) error {
+	bfn := func(kb, vb []byte) bool {
+		kp, vp := uint64ptr(kb), uint64ptr(vb)
+		return fn(*kp, *vp)
+	}
+	return h.m.Iterate(bfn, uint64b(&hint))
+}
+
+func (h *uint64Hashmap) Delete(k uint64) error {
+	return h.m.Delete(uint64b(&k))
+}
+
+func (h *uint64Hashmap) Close() error {
+	return h.m.Close()
+}
+
+func uint32b(v *uint32) []byte {
+	const size = unsafe.Sizeof(*v)
+	return (*[size]byte)(unsafe.Pointer(v))[:]
+}
+
+func uint64b(v *uint64) []byte {
+	const size = unsafe.Sizeof(*v)
+	return (*[size]byte)(unsafe.Pointer(v))[:]
+}
+
+func uint64ptr(b []byte) *uint64 {
+	return (*uint64)(unsafe.Pointer(&b[0]))
+}
+
+func TestArray(t *testing.T) {
+	revert := trackMapFDs(t)
+	defer revert()
+
+	t.Run("Lookup", testArrayLookup)
+	t.Run("OOBAccess", testArrayOOBAccess)
+}
+
+func testArrayLookup(t *testing.T) {
+	a := newUint64Array(t, 8)
+	defer a.Close()
+
+	values := []uint64{4, 8, 15, 16, 23, 42}
+	for i, v := range values {
+		if err := a.Set(uint32(i), v); err != nil {
+			t.Fatal(err)
+		}
+	}
+	for i, v := range values {
+		got, err := a.Lookup(uint32(i))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != v {
+			t.Fatalf("Lookup(%d): got %d, want %d", i, got, v)
+		}
+	}
+}
+
+func testArrayOOBAccess(t *testing.T) {
+	const size = 4
+	a := newUint64Array(t, size)
+	defer a.Close()
+
+	index := uint32(size)
+	v := uint64(5)
+	err := a.Set(index, 0)
+	if err == nil {
+		t.Fatalf("Set(%d, %d) succeeded on array of size %d", index, v, size)
+	}
+	got, err := a.Lookup(index)
+	if err == nil {
+		t.Fatalf("Lookup(%d) succeeded on array of size %d: got %d", index, size, got)
+	}
+}
+
+type uint64Array struct {
+	m *Map
+}
+
+func newUint64Array(t *testing.T, numElements int) *uint64Array {
+	t.Helper()
+	m := &Map{
+		Type:       MapArray,
+		KeySize:    uint32(unsafe.Sizeof(uint32(0))),
+		ValueSize:  uint32(unsafe.Sizeof(uint64(0))),
+		MaxEntries: uint32(numElements),
+		ObjectName: "test_array",
+	}
+	if err := m.Init(); err != nil {
+		t.Fatal(err)
+	}
+	return &uint64Array{m: m}
+}
+
+func (arr *uint64Array) Set(index uint32, value uint64) error {
+	return arr.m.Set(uint32b(&index), uint64b(&value))
+}
+
+func (arr *uint64Array) Lookup(index uint32) (uint64, error) {
+	var value uint64
+	err := arr.m.Lookup(uint32b(&index), uint64b(&value))
+	return value, err
+}
+
+func (arr *uint64Array) Close() error {
+	return arr.m.Close()
+}
+
+// File descriptor tracking utilities.
 
 func trackMapFDs(t *testing.T) (revert func()) {
 	tr := new(mapFDTracker)
